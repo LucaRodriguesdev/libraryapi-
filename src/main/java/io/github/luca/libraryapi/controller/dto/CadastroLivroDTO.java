e1ccccbcc41269028738506e6c72c0d257af0def
lucar <luca2005@gmail.com>
2026-04-11 09:48:13 -0300

    fix: remove validacao ISBN

--- src/main/java/io/github/luca/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/luca/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -4,14 +4,13 @@ import io.github.luca.libraryapi.model.GeneroLivro;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-import org.hibernate.validator.constraints.ISBN;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record CadastroLivroDTO(
-        @ISBN
         @NotBlank(message = "campo obrigatorio")
         String isbn,
         @NotBlank(message = "campo obrigatorio")
